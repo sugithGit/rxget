02ad2971f1484b8880a97da5405fe6cc20f74a91
Sugith K <134681105+sugith10@users.noreply.github.com>
2025-12-18 10:20:26 +0530

    docs: add initial README files to documentation and extensions directories.

--- packages/rxget/lib/src/get_di/get_di.dart
+++ packages/rxget/lib/src/get_di/get_di.dart
@@ -1,5 +1,5 @@
 library;
 
-export '../get_di/src/extension/extension_instance.dart';
 export 'src/di_flutter/get_in.dart';
 export 'src/di_flutter/rx_getin_widget.dart';
+export 'src/extension/extension_instance.dart';
