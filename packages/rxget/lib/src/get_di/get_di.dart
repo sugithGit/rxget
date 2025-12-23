30386e3f295ed650168e90afe7542ba0d3faa8e4
Sugith K <134681105+sugith10@users.noreply.github.com>
2025-12-23 23:59:18 +0530

    fix: type inference issue in GetInWidget

--- packages/rxget/lib/src/get_di/get_di.dart
+++ packages/rxget/lib/src/get_di/get_di.dart
@@ -1,5 +1,6 @@
 library;
 
 export 'src/di_flutter/get_in/get_in.dart';
+export 'src/di_flutter/get_in/get_in_base.dart';
 export 'src/di_flutter/rx_getin_widget.dart';
 export 'src/extension/extension_instance.dart';
